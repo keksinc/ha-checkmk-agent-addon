079beaba62f91992825e7c44dd3ca611c4f16fd0
Sascha Hevelke <maintainer@s.schevelke.de>
2025-06-07 22:14:47 +0200

    wip

--- addon/local.sh
+++ addon/local.sh
@@ -4,13 +4,13 @@ jsonCore=$(curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervi
 jsonOs=$(curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/os/info)
 
 
-coreVersion=$(jq -r ".data.version" "$jsonCore")
-coreLatestVersion=$(jq -r ".data.version_latest" "$jsonCore")
+coreVersion=$(echo "$jsonCore" | jq -r ".data.version")
+coreLatestVersion=$(echo "$jsonCore" | jq -r ".data.version_latest")
 [ "$coreVersion" = "$coreLatestVersion" ] && status=0 || status=1
 echo $status \"HA Core version\" - $coreVersion - Latest: $coreLatestVersion
 
-osVersion=$(jq -r ".data.version" "$jsonOs")
-osLatestVersion=$(jq -r ".data.version_latest" "$jsonOs")
+osVersion=$(echo "$jsonOs" | jq -r ".data.version")
+osLatestVersion=$(echo "$jsonOs" | jq -r ".data.version_latest")
 [ "$osVersion" = "$osLatestVersion" ] && status=0 || status=1
 echo $status \"HA OS version\" - $osVersion - Latest: $osLatestVersion
 
--- addon/run.sh
+++ addon/run.sh
@@ -6,8 +6,8 @@
 jsonCore=$(curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/core/info)
 jsonOs=$(curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/os/info)
 
-jq -r ".data.version" "$jsonCore"
-jq -r ".data.version_latest" "$jsonCore"
+echo "$jsonCore" | jq -r ".data.version"
+echo "$jsonCore" | jq -r ".data.version_latest"
 
 echo "Starting socat"
 /usr/bin/socat -U TCP-LISTEN:6556,fork,reuseaddr EXEC:/bin/check_mk_agent
