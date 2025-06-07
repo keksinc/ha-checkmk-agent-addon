42e17661c50c340a9a12453ec4e62a8522646c10
Sascha Hevelke <maintainer@s.schevelke.de>
2025-06-07 21:43:59 +0200

    wip

--- addon/run.sh
+++ addon/run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
-/bin/check_mk_agent
+#/bin/check_mk_agent
 
 #/usr/bin/ha core info --raw-json | jq -r ".data.version"
 #/usr/bin/ha core info --raw-json | jq -r ".data.version_latest"
 curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/core/info
 curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/os/info
-
+echo ""
+echo token: $SUPERVISOR_TOKEN
 
 echo "Starting socat"
 /usr/bin/socat -U TCP-LISTEN:6556,fork,reuseaddr EXEC:/bin/check_mk_agent
