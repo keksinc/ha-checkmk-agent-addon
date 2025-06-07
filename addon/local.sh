67dc3c22bef41ab7871d715b2c4b23b2a79879f8
Sascha <sascha@MacBook-Pro-von-Sascha-2.local>
2025-06-07 21:19:40 +0200

    added os and core version check

--- addon/local.sh
+++ addon/local.sh
@@ -1,2 +1,12 @@
-echo 0 \"My service\" myvalue=73 My output text which may contain spaces
+coreVersion=$(ha core info --raw-json | jq -r ".data.version")
+coreLatestVersion=$(ha core info --raw-json | jq -r ".data.version_latest")
+[ "$coreVersion" = "$coreLatestVersion" ] && status=0 || status=1
+echo $status \"HA Core version\" - $coreVersion - Latest: $coreLatestVersion
+
+osVersion=$(ha os info --raw-json | jq -r ".data.version")
+osLatestVersion=$(ha os info --raw-json | jq -r ".data.version_latest")
+[ "$osVersion" = "$osLatestVersion" ] && status=0 || status=1
+echo $status \"HA OS version\" - $osVersion - Latest: $osLatestVersion
+
+# echo 0 \"My service\" myvalue=73 My output text which may contain spaces
 
