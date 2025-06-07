62cb1ccba9c37f510da1f0f2e092456499520292
Sascha Hevelke <maintainer@s.schevelke.de>
2025-06-07 22:06:51 +0200

    wip

--- addon/run.sh
+++ addon/run.sh
@@ -4,8 +4,8 @@
 
 #/usr/bin/ha core info --raw-json | jq -r ".data.version"
 #/usr/bin/ha core info --raw-json | jq -r ".data.version_latest"
-curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://10.0.0.179/core/info
-curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://10.0.0.179/os/info
+curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/core/info
+curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/os/info
 echo ""
 
 echo token: $SUPERVISOR_TOKEN
