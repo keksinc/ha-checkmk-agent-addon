b651cf32a30c3b46d1a8858590cabd4559c6551d
Sascha Hevelke <maintainer@s.schevelke.de>
2025-06-07 21:29:01 +0200

    added absolute path for ha binary

--- addon/local.sh
+++ addon/local.sh
@@ -1,13 +1,13 @@
 
 
 
-coreVersion=$(ha core info --raw-json | jq -r ".data.version")
-coreLatestVersion=$(ha core info --raw-json | jq -r ".data.version_latest")
+coreVersion=$(/usr/bin/ha core info --raw-json | jq -r ".data.version")
+coreLatestVersion=$(/usr/bin/ha core info --raw-json | jq -r ".data.version_latest")
 [ "$coreVersion" = "$coreLatestVersion" ] && status=0 || status=1
 echo $status \"HA Core version\" - $coreVersion - Latest: $coreLatestVersion
 
-osVersion=$(ha os info --raw-json | jq -r ".data.version")
-osLatestVersion=$(ha os info --raw-json | jq -r ".data.version_latest")
+osVersion=$(/usr/bin/ha os info --raw-json | jq -r ".data.version")
+osLatestVersion=$(/usr/bin/ha os info --raw-json | jq -r ".data.version_latest")
 [ "$osVersion" = "$osLatestVersion" ] && status=0 || status=1
 echo $status \"HA OS version\" - $osVersion - Latest: $osLatestVersion
 
